9e5705b8ac96956920a922231efeeab3a266b530
jdumasleon <jdumasleon@gmail.com>
2016-05-04 18:01:06 -0400

    Add username and password for server smtp.sendgrid.net

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => ENV['SENDGRID_USERNAME'],
-      :password       => ENV['SENDGRID_PASSWORD'],
+      :user_name      => 'jdumasleon',
+      :password       => 'Alucard@2',
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
