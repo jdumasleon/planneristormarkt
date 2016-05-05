180e1b8cb36e1d1843cd9b69f871d2bdad719b45
jdumasleon <jdumasleon@gmail.com>
2016-05-05 16:23:51 -0400

    Change username and passwrod for SendGrid add-ons

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -85,8 +85,8 @@ Rails.application.configure do
       :address        => 'smtp.sendgrid.net',
       :port           => '587',
       :authentication => :plain,
-      :user_name      => 'jdumasleon',
-      :password       => 'Alucard@2',
+      :user_name      => ENV['SENDGRID_USERNAME'],
+      :password       => ENV['SENDGRID_PASSWORD'],
       :domain         => 'heroku.com',
       :enable_starttls_auto => true
   }
