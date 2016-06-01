8167207de778490d4ad3e8c510dbe26015d08093
jdumasleon <jdumasleon@gmail.com>
2016-06-01 14:11:25 -0400

    Change path items

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -4,188 +4,188 @@ $(document).ready(function() {
   var items = [
     {
        "name" : "AJA0001",
-       "image" : "public/models/thumbnails/tests/AJA0001.jpg",
-       "model" : "public/models/js/tests/AJA0001.js",
+       "image" : "planner/models/thumbnails/tests/AJA0001.jpg",
+       "model" : "planner/models/js/tests/AJA0001.js",
        "type" : "1"
      },
      {
         "name" : "BGB0001",
-        "image" : "public/models/thumbnails/tests/BGB0001.jpg",
-        "model" : "public/models/js/tests/BGB0001.js",
+        "image" : "planner/models/thumbnails/tests/BGB0001.jpg",
+        "model" : "planner/models/js/tests/BGB0001.js",
         "type" : "1"
       },
       {
          "name" : "BHZ0001",
-         "image" : "public/models/thumbnails/tests/BHZ0001.jpg",
-         "model" : "public/models/js/tests/BHZ0001.js",
+         "image" : "planner/models/thumbnails/tests/BHZ0001.jpg",
+         "model" : "planner/models/js/tests/BHZ0001.js",
          "type" : "1"
        },
        {
           "name" : "VS7080GRL",
-          "image" : "public/models/thumbnails/tests/VS7080GRL.jpg",
-          "model" : "public/models/js/tests/VS7080GRL.js",
+          "image" : "planner/models/thumbnails/tests/VS7080GRL.jpg",
+          "model" : "planner/models/js/tests/VS7080GRL.js",
           "type" : "1"
         },
         {
            "name" : "BNC0003",
-           "image" : "public/models/thumbnails/tests/BNC0003.jpg",
-           "model" : "public/models/js/tests/BNC0003.js",
+           "image" : "planner/models/thumbnails/tests/BNC0003.jpg",
+           "model" : "planner/models/js/tests/BNC0003.js",
            "type" : "1"
          },
          {
             "name" : "VS7080FRE17X",
-            "image" : "public/models/thumbnails/tests/VS7080FRE17X.jpg",
-            "model" : "public/models/js/tests/VS7080FRE17X.js",
+            "image" : "planner/models/thumbnails/tests/VS7080FRE17X.jpg",
+            "model" : "planner/models/js/tests/VS7080FRE17X.js",
             "type" : "1"
           },
    {
       "name" : "Closed Door",
-      "image" : "public/models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
-      "model" : "public/models/js/closed-door28x80_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.04.12_PM.png",
+      "model" : "planner/models/js/closed-door28x80_baked.js",
       "type" : "7"
     },
     {
       "name" : "Open Door",
-      "image" : "public/models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.22.46_PM.png",
-      "model" : "public/models/js/open_door.js",
+      "image" : "planner/models/thumbnails/thumbnail_Screen_Shot_2014-10-27_at_8.22.46_PM.png",
+      "model" : "planner/models/js/open_door.js",
       "type" : "7"
     },
     {
       "name" : "Window",
-      "image" : "public/models/thumbnails/thumbnail_window.png",
-      "model" : "public/models/js/whitewindow.js",
+      "image" : "planner/models/thumbnails/thumbnail_window.png",
+      "model" : "planner/models/js/whitewindow.js",
       "type" : "3"
     },
     {
       "name" : "Chair",
-      "image" : "public/models/thumbnails/thumbnail_Church-Chair-oak-white_1024x1024.jpg",
-      "model" : "public/models/js/gus-churchchair-whiteoak.js",
+      "image" : "planner/models/thumbnails/thumbnail_Church-Chair-oak-white_1024x1024.jpg",
+      "model" : "planner/models/js/gus-churchchair-whiteoak.js",
       "type" : "1"
     },
     {
       "name" : "Red Chair",
-      "image" : "public/models/thumbnails/thumbnail_tn-orange.png",
-      "model" : "public/models/js/ik-ekero-orange_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_tn-orange.png",
+      "model" : "planner/models/js/ik-ekero-orange_baked.js",
       "type" : "1"
     },
     {
       "name" : "Blue Chair",
-      "image" : "public/models/thumbnails/thumbnail_ekero-blue3.png",
-      "model" : "public/models/js/ik-ekero-blue_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_ekero-blue3.png",
+      "model" : "planner/models/js/ik-ekero-blue_baked.js",
       "type" : "1"
     },
     {
       "name" : "Dresser - Dark Wood",
-      "image" : "public/models/thumbnails/thumbnail_matera_dresser_5.png",
-      "model" : "public/models/js/DWR_MATERA_DRESSER2.js",
+      "image" : "planner/models/thumbnails/thumbnail_matera_dresser_5.png",
+      "model" : "planner/models/js/DWR_MATERA_DRESSER2.js",
       "type" : "1"
     },
     {
       "name" : "Dresser - White",
-      "image" : "public/models/thumbnails/thumbnail_img25o.jpg",
-      "model" : "public/models/js/we-narrow6white_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_img25o.jpg",
+      "model" : "planner/models/js/we-narrow6white_baked.js",
       "type" : "1"
     },
     {
       "name" : "Bedside table - Shale",
-      "image" : "public/models/thumbnails/thumbnail_Blu-Dot-Shale-Bedside-Table.jpg",
-      "model" : "public/models/js/bd-shalebedside-smoke_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_Blu-Dot-Shale-Bedside-Table.jpg",
+      "model" : "planner/models/js/bd-shalebedside-smoke_baked.js",
       "type" : "1"
     },
     {
       "name" : "Bedside table - White",
-      "image" : "public/models/thumbnails/thumbnail_arch-white-oval-nightstand.jpg",
-      "model" : "public/models/js/cb-archnight-white_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_arch-white-oval-nightstand.jpg",
+      "model" : "planner/models/js/cb-archnight-white_baked.js",
       "type" : "1"
     },
     {
       "name" : "Wardrobe - White",
-      "image" : "public/models/thumbnails/thumbnail_TN-ikea-kvikine.png",
-      "model" : "public/models/js/ik-kivine_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_TN-ikea-kvikine.png",
+      "model" : "planner/models/js/ik-kivine_baked.js",
       "type" : "1"
     },
     {
       "name" : "Full Bed",
-      "image" : "public/models/thumbnails/thumbnail_nordli-bed-frame__0159270_PE315708_S4.JPG",
-      "model" : "public/models/js/ik_nordli_full.js",
+      "image" : "planner/models/thumbnails/thumbnail_nordli-bed-frame__0159270_PE315708_S4.JPG",
+      "model" : "planner/models/js/ik_nordli_full.js",
       "type" : "1"
     },
     {
       "name" : "Bookshelf",
-      "image" : "public/models/thumbnails/thumbnail_kendall-walnut-bookcase.jpg",
-      "model" : "public/models/js/cb-kendallbookcasewalnut_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_kendall-walnut-bookcase.jpg",
+      "model" : "planner/models/js/cb-kendallbookcasewalnut_baked.js",
       "type" : "1"
     },
         {
       "name" : "Media Console - White",
-      "image" : "public/models/thumbnails/thumbnail_clapboard-white-60-media-console-1.jpg",
-      "model" : "public/models/js/cb-clapboard_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_clapboard-white-60-media-console-1.jpg",
+      "model" : "planner/models/js/cb-clapboard_baked.js",
       "type" : "1"
     },
         {
       "name" : "Media Console - Black",
-      "image" : "public/models/thumbnails/thumbnail_moore-60-media-console-1.jpg",
-      "model" : "public/models/js/cb-moore_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_moore-60-media-console-1.jpg",
+      "model" : "planner/models/js/cb-moore_baked.js",
       "type" : "1"
     },
        {
       "name" : "Sectional - Olive",
-      "image" : "public/models/thumbnails/thumbnail_img21o.jpg",
-      "model" : "public/models/js/we-crosby2piece-greenbaked.js",
+      "image" : "planner/models/thumbnails/thumbnail_img21o.jpg",
+      "model" : "planner/models/js/we-crosby2piece-greenbaked.js",
       "type" : "1"
     },
     {
       "name" : "Sofa - Grey",
-      "image" : "public/models/thumbnails/thumbnail_rochelle-sofa-3.jpg",
-      "model" : "public/models/js/cb-rochelle-gray_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_rochelle-sofa-3.jpg",
+      "model" : "planner/models/js/cb-rochelle-gray_baked.js",
       "type" : "1"
     },
         {
       "name" : "Wooden Trunk",
-      "image" : "public/models/thumbnails/thumbnail_teca-storage-trunk.jpg",
-      "model" : "public/models/js/cb-tecs_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_teca-storage-trunk.jpg",
+      "model" : "planner/models/js/cb-tecs_baked.js",
       "type" : "1"
     },
         {
       "name" : "Floor Lamp",
-      "image" : "public/models/thumbnails/thumbnail_ore-white.png",
-      "model" : "public/models/js/ore-3legged-white_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_ore-white.png",
+      "model" : "planner/models/js/ore-3legged-white_baked.js",
       "type" : "1"
     },
     {
       "name" : "Coffee Table - Wood",
-      "image" : "public/models/thumbnails/thumbnail_stockholm-coffee-table__0181245_PE332924_S4.JPG",
-      "model" : "public/models/js/ik-stockholmcoffee-brown.js",
+      "image" : "planner/models/thumbnails/thumbnail_stockholm-coffee-table__0181245_PE332924_S4.JPG",
+      "model" : "planner/models/js/ik-stockholmcoffee-brown.js",
       "type" : "1"
     },
     {
       "name" : "Side Table",
-      "image" : "public/models/thumbnails/thumbnail_Screen_Shot_2014-02-21_at_1.24.58_PM.png",
-      "model" : "public/models/js/GUSossingtonendtable.js",
+      "image" : "planner/models/thumbnails/thumbnail_Screen_Shot_2014-02-21_at_1.24.58_PM.png",
+      "model" : "planner/models/js/GUSossingtonendtable.js",
       "type" : "1"
     },
     {
       "name" : "Dining Table",
-      "image" : "public/models/thumbnails/thumbnail_scholar-dining-table.jpg",
-      "model" : "public/models/js/cb-scholartable_baked.js",
+      "image" : "planner/models/thumbnails/thumbnail_scholar-dining-table.jpg",
+      "model" : "planner/models/js/cb-scholartable_baked.js",
       "type" : "1"
     },
     {
       "name" : "Dining table",
-      "image" : "public/models/thumbnails/thumbnail_Screen_Shot_2014-01-28_at_6.49.33_PM.png",
-      "model" : "public/models/js/BlakeAvenuejoshuatreecheftable.js",
+      "image" : "planner/models/thumbnails/thumbnail_Screen_Shot_2014-01-28_at_6.49.33_PM.png",
+      "model" : "planner/models/js/BlakeAvenuejoshuatreecheftable.js",
       "type" : "1"
     },
     {
       "name" : "Blue Rug",
-      "image" : "public/models/thumbnails/thumbnail_cb-blue-block60x96.png",
-      "model" : "public/models/js/cb-blue-block-60x96.js",
+      "image" : "planner/models/thumbnails/thumbnail_cb-blue-block60x96.png",
+      "model" : "planner/models/js/cb-blue-block-60x96.js",
       "type" : "8"
     },
     {
       "name" : "NYC Poster",
-      "image" : "public/models/thumbnails/thumbnail_nyc2.jpg",
-      "model" : "public/models/js/nyc-poster2.js",
+      "image" : "planner/models/thumbnails/thumbnail_nyc2.jpg",
+      "model" : "planner/models/js/nyc-poster2.js",
       "type" : "2"
     }
    /*
